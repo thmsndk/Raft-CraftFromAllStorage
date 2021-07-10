b5117beb0ae1fe58f7f4eab63bc9af43b5eac48c
thmsn <9084377+thmsndk@users.noreply.github.com>
2021-07-10 23:24:05 +0200

    Extracted inventorymanger to it's own file

---
+++ CraftFromAllStorage/InventoryManager.cs
@@ -0,0 +1,18 @@
+﻿class InventoryManager
+{
+    static public Inventory GetPlayerInventory()
+    {
+        return RAPI.GetLocalPlayer().Inventory;
+    }
+
+    /// <summary>
+    /// Gets the currently open storage inventory for the player
+    /// </summary>
+    /// <returns></returns>
+    static public Inventory GetCurrentStorageInventory()
+    {
+        return RAPI.GetLocalPlayer().StorageManager.currentStorage?.GetInventoryReference();
+    }
+}
+
+// Patches for Auto Recipe Redux support
