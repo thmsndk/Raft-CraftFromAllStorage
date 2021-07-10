8f5ff5ef1eabe100be26b8043251997bc3c08a88
thmsn <9084377+thmsndk@users.noreply.github.com>
2021-07-11 01:28:47 +0200

    Added support for Auto Recipe Redux

--- CraftFromAllStorage/HarmonyPatches.cs
+++ CraftFromAllStorage/HarmonyPatches.cs
@@ -1,6 +1,8 @@
 ﻿using HarmonyLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
@@ -58,7 +60,7 @@ class SetAmountInInventoryPatch
             List<Item_Base> items = CraftFromStorageManager.getItemsFromCostBox(__instance);
 
             var currentStorageInventory = InventoryManager.GetCurrentStorageInventory();
-            
+
             int storageInventoryAmount = 0;
             foreach (var costBoxItem in items)
             {
@@ -96,19 +98,152 @@ class RemoveCost
     }
 }
 
-class InventoryManager
-{
-    static public Inventory GetPlayerInventory()
-    {
-        return RAPI.GetLocalPlayer().Inventory;
-    }
+// Patches for Auto Recipe Redux support
 
-    /// <summary>
-    /// Gets the currently open storage inventory for the player
-    /// </summary>
-    /// <returns></returns>
-    static public Inventory GetCurrentStorageInventory()
+/// <summary>
+/// This needs to be overriden to indicate we have enough resources
+/// </summary>
+[HarmonyPatch(typeof(Inventory), "GetItemCount", typeof(Item_Base))]
+class GetItemCount
+{
+    static void Postfix(PlayerInventory __instance, ref int __result, Item_Base item)
     {
-        return RAPI.GetLocalPlayer().StorageManager.currentStorage?.GetInventoryReference();
+        //// https://stackoverflow.com/a/615976/28145
+        //System.Diagnostics.StackTrace stackTrace = new System.Diagnostics.StackTrace();
+        //MethodBase methodBase = stackTrace.GetFrame(1).GetMethod();
+        //Debug.Log("GetItemCount: " + methodBase.);
+
+        // Handle both fuel counts and item counts from AutoRecipeBehaviour.CalculatePreparation (colors resources red if missing)
+        if (!Environment.StackTrace.Contains("at AutoRecipeBehaviour.OnIsRayed"))
+        {
+            return;
+        }
+
+        // Append item count of storages to original GetItemCount
+        if (!CraftFromStorageManager.HasUnlimitedResources())
+        {
+            foreach (Storage_Small storage in StorageManager.allStorages)
+            {
+                Inventory container = storage.GetInventoryReference();
+                if (storage.IsOpen || container == null /*|| !Helper.LocalPlayerIsWithinDistance(storage.transform.position, player.StorageManager.maxDistanceToStorage)*/)
+                    continue;
+
+                __result += container.GetItemCountWithoutDuplicates(item.UniqueName);
+            }
+        }
+    }
+}
+
+/// <summary>
+/// Patch RemoveItem so fuel can be removed from storage as well as the players inventory.
+/// this.cookingPot.localPlayer.Inventory.RemoveItem(this.cookingPot.Fuel.fuelItem.UniqueName, fuel);
+/// </summary>
+[HarmonyPatch(typeof(Inventory), "RemoveItem", typeof(string), typeof(int))]
+class RemoveItem
+{
+    static bool Prefix(PlayerInventory __instance, string uniqueItemName, int amount)
+    {
+        if (!Environment.StackTrace.Contains("at AutoRecipeBehaviour.OnIsRayed"))
+        {
+            return true;
+        }
+
+        // Append item count of storages to original GetItemCount
+        if (!CraftFromStorageManager.HasUnlimitedResources())
+        {
+            // Get count in player inventory
+            var playerInventoryCount = __instance.GetItemCount(uniqueItemName); // we have only patched the overload that uses the item, so this should not trigger our patch
+
+            var amountToRemoveFromPlayerInventory = amount - Math.Min(playerInventoryCount, amount);
+            var amountToRemove = amountToRemoveFromPlayerInventory;
+
+            if (amountToRemove <= 0)
+            {
+                return true;
+            }
+
+            foreach (Storage_Small storage in StorageManager.allStorages)
+            {
+                Inventory container = storage.GetInventoryReference();
+                if (storage.IsOpen || container == null /*|| !Helper.LocalPlayerIsWithinDistance(storage.transform.position, player.StorageManager.maxDistanceToStorage)*/)
+                    continue;
+
+                var containerItemCount = container.GetItemCountWithoutDuplicates(uniqueItemName);
+                var amountToRemoveFromContainer = Math.Min(containerItemCount, amountToRemove);
+
+                container.RemoveItemUses(uniqueItemName, amountToRemoveFromContainer, false);
+                amountToRemove -= amountToRemoveFromContainer;
+
+                if (amountToRemove <= 0)
+                {
+                    // All items have been removed.
+                    break;
+                }
+            }
+
+            return amountToRemoveFromPlayerInventory > 0; // only run the original method if player inventory has something to remove.
+        }
+
+        return true;
+    }
+}
+
+/// <summary>
+/// Allow Inserting item to CookingTable_Slot to pull from player inventory as well as other storages.
+/// </summary>
+[HarmonyPatch(typeof(CookingTable_Slot), "InsertItem")]
+class InsertItem
+{
+    static bool Prefix(CookingTable_Slot __instance,
+                       Network_Player player,
+                       ItemInstance itemInstance,
+                       ref ItemInstance ___currentItem, // private field
+                       ItemObjectEnabler ___objectEnabler, // private field
+                       CookingTable ___cookingPot) // private field
+    {
+        ___currentItem = itemInstance.Clone();
+        ___currentItem.Amount = 1;
+        ___objectEnabler.ActivateModel(___currentItem.baseItem);
+        if (player != null && player.IsLocalPlayer)
+        {
+            var remainingUses = itemInstance.Uses;
+            var playerInventoryCount = player.Inventory.GetItemCount(itemInstance.UniqueName);
+
+            var amountToRemoveFromPlayerInventory = Math.Min(playerInventoryCount, remainingUses);
+
+            player.Inventory.RemoveItemUses(itemInstance.UniqueName, amountToRemoveFromPlayerInventory, false);
+            remainingUses -= amountToRemoveFromPlayerInventory;
+
+            if (remainingUses <= 0)
+            {
+                return false;
+            }
+
+            foreach (Storage_Small storage in StorageManager.allStorages)
+            {
+                Inventory container = storage.GetInventoryReference();
+                if (storage.IsOpen || container == null /*|| !Helper.LocalPlayerIsWithinDistance(storage.transform.position, player.StorageManager.maxDistanceToStorage)*/)
+                {
+                    continue;
+                }
+
+                var containerItemCount = container.GetItemCountWithoutDuplicates(itemInstance.UniqueName);
+                var amountToRemoveFromContainer = Math.Min(containerItemCount, remainingUses);
+
+                container.RemoveItemUses(itemInstance.UniqueName, amountToRemoveFromContainer, false);
+                remainingUses -= amountToRemoveFromContainer;
+
+                if (remainingUses <= 0)
+                {
+                    // All items have been removed.
+                    break;
+                }
+            }
+
+            ___cookingPot.displayText.HideDisplayTexts();
+            player.Animator.SetAnimation(PlayerAnimation.Trigger_Plant, false);
+        }
+
+        return false;
     }
 }
