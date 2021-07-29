ae8895dbbc0db1d70ec9f3baa0cb066b9ecf3b33
thmsn <9084377+thmsndk@users.noreply.github.com>
2021-07-29 20:13:09 +0200

    Potentially fix duplication/sync issues by only broadcasting if you are the host and sending if you are not the host.

--- CraftFromAllStorage/ExtentionMethods.cs
+++ CraftFromAllStorage/ExtentionMethods.cs
@@ -105,7 +105,17 @@ static class ExtentionMethods
 
     public static void BroadcastCloseEvent(this Storage_Small box)
     {
-        new Message_Storage_Close(Messages.StorageManager_Close, RAPI.GetLocalPlayer().StorageManager, box).Broadcast(NetworkChannel.Channel_Game);
+        var player = RAPI.GetLocalPlayer();
+        var message = new Message_Storage_Close(Messages.StorageManager_Close, player.StorageManager, box);
+        if (Semih_Network.IsHost)
+        {
+            // TODO. if we are sending multiple messages, we could potentially batch them together? e.g. removing items from multiple storages.
+            message.Broadcast(NetworkChannel.Channel_Game);
+        }
+        else
+        {
+            message.Send(player.Network.HostID, NetworkChannel.Channel_Game);
+        }
     }
 
     //public static bool Contains(this List<ItemInstance> items, Item_Base item)
@@ -461,5 +471,5 @@ static class ExtentionMethods
     //}
 
     public static void Broadcast(this Message message, NetworkChannel channel/* = (NetworkChannel)MessageType.ChannelID*/) => ComponentManager<Semih_Network>.Value.RPC(message, Target.Other, EP2PSend.k_EP2PSendReliable, channel);
-    //public static void Send(this Message message, CSteamID steamID) => ComponentManager<Semih_Network>.Value.SendP2P(steamID, message, EP2PSend.k_EP2PSendReliable, (NetworkChannel)MessageType.ChannelID);
+    public static void Send(this Message message, CSteamID steamID, NetworkChannel channel) => ComponentManager<Semih_Network>.Value.SendP2P(steamID, message, EP2PSend.k_EP2PSendReliable, channel);
 }
